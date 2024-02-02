09099b73e74049877e7560c7606abe1faad48276
ExpertSneeker <ygzxmd@gmail.com>
2024-02-02 21:45:02 +0800

    优化方法名，分离loreKeywordHand方法

--- src/main/java/com/coreages/coreagescmd/util/LoreUtils.java
+++ src/main/java/com/coreages/coreagescmd/util/LoreUtils.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,8 +17,9 @@ import java.util.List;
  * @Create 2024/1/30 20:57
  * @Version 1.0
  */
-public class LoreHasKey {
-    public boolean containsLoreKeyword(Player player, String keyword) {
+public class LoreUtils {
+    //检测装备是否包含lore
+    public boolean loreKeywordArmor(Player player, String keyword) {
         PlayerInventory inventory = player.getInventory();
 
         // 检查所有装备槽位（头盔、胸甲、护腿、靴子）
@@ -35,7 +37,32 @@ public class LoreHasKey {
                 }
             }
         }
-
         return false; // 没有找到含有关键字的lore
     }
+
+    public boolean loreKeywordHand(ItemStack item, List<String> words){
+
+        //获取物品meta
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null && meta.hasLore()) {
+            //获取物品的lore
+            List<String> lore = meta.getLore();
+
+            // 检查lore中是否包含特定的字符串
+            if (lore != null) {
+                for (String line : lore) {
+                    for (String keyword : words) {
+                        if (line.contains(keyword)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;//不包含
+    }
+
+    
 }
